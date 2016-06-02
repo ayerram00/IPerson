d1cb93bc19a4b0e781530b18d0047f10c24e5497
Anand Yerramsetty <anand.shopping@gmail.com>
2016-06-02 15:00:49 -0700

    adding actual code files

---
+++ src/main/java/com/sqa/ay/IPerson/IPerson.java
@@ -0,0 +1,6 @@
+
+package com.sqa.ay.IPerson;
+
+public interface IPerson {
+
+}
---
+++ src/test/java/com/sqa/ay/IPerson/IPersonTest.java
@@ -0,0 +1,8 @@
+
+package com.sqa.ay.IPerson;
+
+import junit.framework.*;
+
+public class IPersonTest extends TestCase {
+
+}
